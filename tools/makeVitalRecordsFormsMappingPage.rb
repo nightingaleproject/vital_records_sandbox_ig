198057e3871df439f61ea9a2c723d8de5f6658f8
Patricia Tran <PTRAN@MITRE.ORG>
2024-01-23 16:19:20 -0500

    forms mapping page and updated intros for vrdr

--- tools/makeVitalRecordsFormsMappingPage.rb
+++ tools/makeVitalRecordsFormsMappingPage.rb
@@ -1,24 +1,29 @@
-# ruby tools/makeVitalRecordsFormsMappingPage.rb input/mapping/BFDR_Forms_Mapping.xlsx
-# output file: generated/bfdr/vital_records_forms_mapping.md
+# ruby tools/makeVitalRecordsFormsMappingPage.rb input/mapping/BFDR_Forms_Mapping.csv
+# output file: generated/vrdr/vrdr_forms_mapping.md
 
 require "json"
+require 'open-uri'
 require "pry"
 require "roo"
+require "csv"
+
+# download2 = URI.open('https://github.com/nightingaleproject/vital_records_sandbox_ig/blob/main/input/mapping/BFDR_Forms_Mapping.csv?raw=true')
+# IO.copy_stream(download2, 'input/mapping/BFDR_Forms_Mapping.csv')
 
 def get_file_type(file)
   File.extname(file).gsub(".", "")
 end
 
-def open_spreadsheet(file)
-  extension = get_file_type(file)
-  if ["csv", "xls", "xlsx"].include? extension
-    Roo::Spreadsheet.open(file, extension: extension)
-  else
-    raise "Unknown file type: #{file}"
-  end
-end
+# def open_spreadsheet(file)
+#   extension = get_file_type(file)
+#   if ["csv", "xls", "xlsx"].include? extension
+#     Roo::Spreadsheet.open(file, extension: extension)
+#   else
+#     raise "Unknown file type: #{file}"
+#   end
+# end
 
-# BFDR_Forms_mapping.xlsx columns
+# BFDR_Forms_mapping.csv columns
 FORMS_ORDER_COL = 0
 FORMS_FORM_COL = 1
 FORMS_URL_COL = 2
@@ -30,100 +35,133 @@ FORMS_FIELD_COL = 7
 FORMS_CONTEXT_COL = 8
 FORMS_QAIRE_COL = 9
 FORMS_QAIRE_FIELD_COL = 10
+FORMS_MAPPING_IG_COL = 12
+FORMS_COMP_NAME_COL = 13
 
 puts ARGV[0]
+vSpreadsheet = ARGV[0]
+# vSpreadsheet = open_spreadsheet(ARGV[0])
+# vSpreadsheet.default_sheet = "BFDR Form Items"
 
-vSpreadsheet = open_spreadsheet(ARGV[0])
-vSpreadsheet.default_sheet = "BFDR Form Items"
-
-vOutputFilename = "generated/BFDR/vital_records_forms_mapping.md"
+vOutputFilename = "generated/VRDR/vrdr_forms_mapping.md"
 puts vOutputFilename
 vOutputFile = File.open(vOutputFilename, "w")
 
-vOutputFile.puts "This IG supports communicating information from an EHR system to the jurisdictional vital records offices and to NCHS for standard reporting forms:
+vOutputFile.puts"<style>
+    table.style1 { 
+        border-collapse: collapse; 
+        width: 100%; 
+        table-layout: fixed;
+    }  
+    table.style1 tbody tr {
+    border-bottom: 1px solid #dddddd;
+    } 
+    table.style1 tbody tr:nth-of-type(even) { 
+        background-color: #f3f3f3; 
+    } 
+    table.style1 tbody tr:last-of-type {
+    border-bottom: 2px solid #98c1d9;
+    }
+    </style>"
 
-**Live Birth**
- * [2003 Revision of the U.S. Standard Certificate of Live Birth](https://www.cdc.gov/nchs/data/dvs/birth11-03final-ACC.pdf)
- * [2016 US Standard Attachment to the Facility Worksheet for the Live Birth Certificate for Multiple Births](https://www.cdc.gov/nchs/data/dvs/multiple-births-worksheet-2016.pdf)
- * [2016 US Standard Facility Worksheet for the Live Birth Certificate](https://www.cdc.gov/nchs/data/dvs/facility-worksheet-2016-508.pdf)
- * [2016 US Standard Mothers Worksheet for Child’s Birth Certificate](https://www.cdc.gov/nchs/data/dvs/moms-worksheet-2016-508.pdf)
- 
-**Fetal Death**
- * [2003 Revision of the U.S. Standard Report of Fetal Death](https://www.cdc.gov/nchs/data/dvs/FDEATH11-03finalACC.pdf)
- * [2019 US Standard Facility Worksheet for the Report of Fetal Death](https://www.cdc.gov/nchs/data/dvs/fetal-death-facility-worksheet-2019-508.pdf)
- * [2019 US Standard Patient’s Worksheet for the Report of Fetal Death](https://www.cdc.gov/nchs/data/dvs/fetal-death-mother-worksheet-english-2019-508.pdf)
- 
-The following tables map the form elements to the appropriate profile or extension along with the containing specification:
-* BFDR: Vital Records Birth and Fetal Death Reporting (this IG)
-* VRCPL: [Vital Records Common Profile Library]({{site.data.fhir.ver.hl7fhirusvrcommonlibrary}})
-* US CORE: [US Core Implementation Guide, 5.0.1]({{site.data.fhir.ver.hl7fhiruscore}})
-* ODH: [Occupational Data for Health]({{site.data.fhir.ver.hl7fhirusodh}})
-* FHIR: [extensions](http://hl7.org/fhir/extensions/extension-registry.html)
+vOutputFile.puts "This page provides the mapping from standard forms and worksheets used to exchange death information to the FHIR resources as defined in this IG.
 
-The Profile/Extension column contains a link to the profile or extension that contains the specific data element. Where there are dot separated pieces in the link, the bolded part of the link is the target. The non-bolded parts of the link are for context:
- * e.g. [Encounter-birth.location.location.**us-core-location**.location.location]({{site.data.fhir.ver.hl7fhiruscore}}/StructureDefinition-us-core-location.html)
-   * the specific data being mapped is recorded in the US Core Location profile and the link resolves to that profile
-   * the US Core Location profile is referenced in the BFDR Encounter-birth profile in Encounter.location.location
-
-The last two tables on this page map the Patient's Worksheets to the Questionnaires.
+This IG supports communicating information from an EHR system to the jurisdictional vital records offices and to NCHS for standard reporting forms:
+ * [U.S. Standard Certificate Of Death](https://www.cdc.gov/nchs/data/dvs/DEATH11-03final-ACC.pdf) ([see table](vital_records_form_mapping.html#us-standard-certificate-of-death-mapping))
 
 Information on updates to the live birth and fetal death forms can be found at NVSS [Revisions of the U.S. Standard Certificates and Reports](https://www.cdc.gov/nchs/nvss/revisions-of-the-us-standard-certificates-and-reports.htm) and [Guide to Completing the Facility Worksheets for the Certificate of Live Birth and Report of Fetal Death](https://www.cdc.gov/nchs/nvss/facility-worksheets-guide.htm)"
 
 vOutputFile.puts ""
 
-def createMappingTable(pRowFilter, pOutputFile, pSpreadsheet)
+def createMappingTable(pRowFilter, pRowFilterLink, pOutputFile, pSpreadsheet)
 
-    pOutputFile.puts "### " + pRowFilter + " Mapping"
+    igMap = Hash.new
+    # can change this to reference older BFDR version
+    #igMap["BFDR"] = "{{site.data.fhir.ver.hl7fhirusbfdr}}"
+    igMap["VRDR"] = ""
+    igMap["VRCPL"] = "{{site.data.fhir.ver.hl7fhirusvrcommonlibrary}}/"
+    igMap["US CORE"] = "{{site.data.fhir.ver.hl7fhiruscore}}/"
+    igMap["FHIR"] = "http://hl7.org/fhir/extensions/"
+    igMap["ODH"] = "{{site.data.fhir.ver.hl7fhirusodh}}"
+    
+    pOutputFile.puts "### " + "<a href='#{pRowFilterLink}'>#{pRowFilter} Mapping</a>" 
     pOutputFile.puts ""
+    pOutputFile.puts "<table  align='left' border='1' class='style1' cellpadding='1' cellspacing='1'>"
+    pOutputFile.puts "<thead>"
+    pOutputFile.puts "  <tr>"
+    pOutputFile.puts "    <td style='background-color:#98c1d9; text-align: center; width: 5%;'><b>Item #</b></td>"
+    pOutputFile.puts "    <td style='background-color:#98c1d9; width: 25%;'><b>Form Element</b></td>"
+    pOutputFile.puts "    <td style='background-color:#98c1d9; width: 25%;'><b>FHIR Profile</b></td>"
+    pOutputFile.puts "    <td style='background-color:#98c1d9; width: 20%;'><b>FHIR Field</b></td>"
+    pOutputFile.puts "  </tr>"
+    pOutputFile.puts "</thead>"
+    pOutputFile.puts "<tbody>"
     
-    pOutputFile.puts "| **Form** | **Element** | **Profile/Extension** | **Location** |"
-    pOutputFile.puts "| -------- | ----------- | ----------- | ------------ |"
-    
-    pSpreadsheet.each_row_streaming(offset:1, pad_cells: true) do |row|
-    
+    CSV.foreach(pSpreadsheet) do |row|
       next if row[FORMS_FORM_COL].to_s != pRowFilter || row[FORMS_PROFILE_COL].to_s == "not implemented"
-    
-      vForm = vFormURL = vFormWithUrl = vElement = vProfile = vIg =vField = vContext = ""
 
-      vForm = "[" + row[FORMS_FORM_COL].to_s + "]" if row[FORMS_FORM_COL]
-      vFormURL = "(" + row[FORMS_URL_COL].to_s + ")" if row[FORMS_URL_COL]
-      vFormWithUrl = vForm + vFormURL
+      vElement = vProfile =vField = vContext = vIG = vExtension = vMappingIG = ""
 
-      vElement = row[FORMS_ELEMENT_COL].to_s if row[FORMS_ELEMENT_COL]
+      formsElement = row[FORMS_ELEMENT_COL].to_s
+      if formsElement.include? ". "
+        formsElements = formsElement.strip.split(" ", 2)
+        vItemNum = formsElements[0].chomp(".")
+        vItemName = formsElements[1]
+      else
+        vItemNum = "-"
+        vItemName = formsElement
+      end
 
       vIg = row[FORMS_IG_COL].to_s if row[FORMS_IG_COL]
-      vProfile = row[FORMS_PROFILE_COL].to_s if row[FORMS_PROFILE_COL]
+      vMappingIg = row[FORMS_MAPPING_IG_COL].to_s if row[FORMS_MAPPING_IG_COL]
+      vProfile = row[FORMS_MAPPING_PROFILE_COL].to_s if row[FORMS_MAPPING_PROFILE_COL]
+      vProfileName = row[FORMS_COMP_NAME_COL].to_s if row[FORMS_COMP_NAME_COL]
       
       # There's some weirdness with the Roo gem and empty and nil fields - hence double to_s and check for empty hack
-      vField = "." + row[FORMS_FIELD_COL].to_s if !row[FORMS_FIELD_COL].to_s.to_s.empty?
-      vContext = row[FORMS_CONTEXT_COL].to_s + "." if !row[FORMS_CONTEXT_COL].to_s.to_s.empty?
+      vContext = row[FORMS_CONTEXT_COL].to_s if !row[FORMS_CONTEXT_COL].to_s.to_s.empty?
+      vField = row[FORMS_FIELD_COL].to_s.empty? ? row[FORMS_CONTEXT_COL].to_s.to_s.partition('.').last : row[FORMS_FIELD_COL].to_s
 
-      vProfileFieldContext = vContext + "**" + vProfile + "**" + vField
-
-      case vIg
-        when "BFDR"
-            if vProfile.include?("Questionnaire") 
-              vProfileWithURL = "[" + vProfileFieldContext + "]" + "(Questionnaire-" + vProfile + ".html)"  
-            else
-              vProfileWithURL = "[" + vProfileFieldContext + "]" + "({{site.data.fhir.ver.hl7fhirusbfdr}}/" + "StructureDefinition-" + vProfile + ".html)"
-            end
-        when "VRCPL"
-            # *TODO* this may needneeds to be updated prior to publication - for some reason the jekyll variable doesn't work the same for current build IG
-            #vProfileWithURL = "[" + vProfile + "]" + "({{site.data.fhir.ver.hl7fhirusvrcommonlibrary}}" + "/StructureDefinition/" + vProfile + ")"
-            vProfileWithURL = "[" + vProfileFieldContext + "]" + "({{site.data.fhir.ver.hl7fhirusvrcommonlibrary}}/" + "StructureDefinition-" + vProfile + ".html)"
-        when "US CORE"
-            vProfileWithURL = "[" + vProfileFieldContext + "]" + "({{site.data.fhir.ver.hl7fhiruscore}}" + "/StructureDefinition-" + vProfile + ".html)"
-        when "FHIR"
-            vProfileWithURL = "[" + vProfileFieldContext + "]" + "(http://hl7.org/fhir/extensions/StructureDefinition-" + vProfile + ".html)"
-        when "ODH"
-            vProfileWithURL = "[" + vProfileFieldContext + "]" + "({{site.data.fhir.ver.hl7fhirusodh}}" + "/StructureDefinition-" + vProfile + ".html)"
-        else
-            vProfileWithURL = ""
+      hasContext = !row[FORMS_CONTEXT_COL].nil?
+      # if there is a "context" filled in for the sheet,
+      # make sure that the proper IG is used (mapping IG)
+      if hasContext && row[FORMS_FIELD_COL].nil?
+        vFieldProfile = row[FORMS_PROFILE_COL].to_s if row[FORMS_PROFILE_COL]
+        if vFieldProfile.nil?
+          puts "- Profile column is empty - "
+        end
+        vField = "<a href='#{igMap[vIg]}" + "StructureDefinition-" + "#{vFieldProfile}" + ".html'>#{vField}</a>"  
+        
+        # vField = "[" + vField + "]" + "("+ igMap[vIg] + "StructureDefinition-" + vFieldProfile + ".html)"
+        if vMappingIg.nil?
+          puts "- Mapping IG column is empty for profile - "
+          puts vContext
+        end
+        vProfileWithURL = "<a href='#{igMap[vMappingIg]}" + "StructureDefinition-" + "#{vProfile}" + ".html'>#{vProfileName}</a>"  
+        # vProfileWithURL = "[" + vProfile + "]" + "("+ igMap[vMappingIg] + "StructureDefinition-" + vProfile + ".html)"
+      elsif hasContext
+        vProfileWithURL = "<a href='#{igMap[vMappingIg]}" + "StructureDefinition-" + "#{vProfile}" + ".html'>#{vProfileName}</a>"  
+        vField = "<a href='#{igMap[vIg]}" + "StructureDefinition-" + "#{vFieldProfile}" + ".html'>#{vField}</a>"  
+        # vProfileWithURL = "[" + vProfile + "]" + "("+ igMap[vMappingIg] + "StructureDefinition-" + vProfile + ".html)"
+      else
+        vProfileWithURL = "<a href='#{igMap[vIg]}" + "StructureDefinition-" + "#{vProfile}" + ".html'>#{vProfileName}</a>"  
+        # vProfileWithURL = "[" + vProfile + "]" + "("+ igMap[vIg] + "StructureDefinition-" + vProfile + ".html)"
       end
-      
-      pOutputFile.puts "| " + vFormWithUrl + " | " + vElement + " | " + vProfileWithURL + " | " + vIg + " |"
+
+      if vProfile.include?("Questionnaire") 
+        vProfileWithURL = "<a href='Questionnaire-#{vProfile}"+".html'>#{vProfileName}</a>"  
+        # vProfileWithURL = "[" + vProfile + "]" + "(Questionnaire-" + vProfile + ".html)" 
+      end
+      pOutputFile.puts "<tr>"
+      pOutputFile.puts "  <td style='text-align: center'>" + vItemNum + "</td>"
+      pOutputFile.puts "  <td>" + vItemName + "</td>"
+      pOutputFile.puts "  <td>" + vProfileWithURL + "</td>"
+      pOutputFile.puts "  <td>" + vField + "</td>"
+      pOutputFile.puts "</tr>"
+      # pOutputFile.puts "| " + vItemNum + " | " + vItemName + " | " + vProfileWithURL + " | " + vField + " |"
+
     end
-    pOutputFile.puts "{: .grid }"
+    pOutputFile.puts "</tbody>"
+    pOutputFile.puts "</table>"
   end
 
 # Adding extra processing to map to the Questionnaires
@@ -131,19 +169,32 @@ def createMappingTableQaire(pRowFilter, pOutputFile, pSpreadsheet)
 
   pOutputFile.puts "### " + pRowFilter + " Questionnaire Mapping"
   pOutputFile.puts ""
+  pOutputFile.puts "<table  align='left' border='1' class='style1' cellpadding='1' cellspacing='1'>"
+  pOutputFile.puts "<thead>"
+  pOutputFile.puts "  <tr>"
+  pOutputFile.puts "    <th style='background-color:#98c1d9; text-align: center; width: 5%;'><b>Item #</b></th>"
+  pOutputFile.puts "    <th style='background-color:#98c1d9; width: 25%;'><b>Form Element</b></th>"
+  pOutputFile.puts "    <th style='background-color:#98c1d9; width: 25%;'><b>Questionnaire</b></th>"
+  pOutputFile.puts "    <th style='background-color:#98c1d9; width: 20%;'><b>FHIR Field</b></th>"
+  pOutputFile.puts "  </tr>"
+  pOutputFile.puts "</thead>"
+  pOutputFile.puts "<tbody>"
   
-  pOutputFile.puts "| **Form** | **Element** | **Questionnaire** | **Location** |"
-  pOutputFile.puts "| -------- | ----------- | ----------- | ------------ |"
-  
-  pSpreadsheet.each_row_streaming(offset:1, pad_cells: true) do |row|
+  CSV.foreach(pSpreadsheet) do |row|
   
     next if row[FORMS_FORM_COL].to_s != pRowFilter || row[FORMS_PROFILE_COL].to_s == "not implemented"
   
-    vForm = vFormURL = vFormWithUrl = vElement = vQuestionnaire = vQuestionnaireWithField = vIg = vField = ""
+    vElement = vQuestionnaire = vQuestionnaireWithField = vField = ""
 
-    vForm = "[" + row[FORMS_FORM_COL].to_s + "]" if row[FORMS_FORM_COL]
-    vFormURL = "(" + row[FORMS_URL_COL].to_s + ")" if row[FORMS_URL_COL]
-    vFormWithUrl = vForm + vFormURL
+    formsElement = row[FORMS_ELEMENT_COL].to_s
+    if formsElement.include? ". "
+      formsElements = formsElement.strip.split(" ", 2)
+      vItemNum = formsElements[0].chomp(".")
+      vItemName = formsElements[1]
+    else
+      vItemNum = "-"
+      vItemName = formsElement
+    end
 
     vElement = row[FORMS_ELEMENT_COL].to_s if row[FORMS_ELEMENT_COL]
 
@@ -154,22 +205,22 @@ def createMappingTableQaire(pRowFilter, pOutputFile, pSpreadsheet)
     vField = "." + row[FORMS_QAIRE_FIELD_COL].to_s if !row[FORMS_QAIRE_FIELD_COL].to_s.to_s.empty?
     
     vQuestionnaireWithField = vQuestionnaire + vField
-    vQuestionnaireWithURL = "[" + vQuestionnaireWithField + "]" + "(Questionnaire-" + vQuestionnaire + ".html)"  
+    vQuestionnaireWithURL =  "<a href='Questionnaire-#{vQuestionnaire}"+".html'>#{vQuestionnaireWithField}</a>"  
+    # vQuestionnaireWithURL = "[" + vQuestionnaireWithField + "]" + "(Questionnaire-" + vQuestionnaire + ".html)"  
         
-    pOutputFile.puts "| " + vFormWithUrl + " | " + vElement + " | " + vQuestionnaireWithURL + " | " + vIG + " |"
+    pOutputFile.puts "<tr>"
+    pOutputFile.puts "  <td style='text-align: center'>" + vItemNum + "</td>"
+    pOutputFile.puts "  <td>" + vItemName + "</td>"
+    pOutputFile.puts "  <td>" + vQuestionnaireWithURL + "</td>"
+    pOutputFile.puts "  <td>" + vField[1..-1] + "</td>"
+    pOutputFile.puts "</tr>"
+    # pOutputFile.puts "| " + vItemNum + " | " + vItemName + " | " + vQuestionnaireWithURL + " | " + vField[1..-1] + " |"
+
   end
-  pOutputFile.puts "{: .grid }"
+  pOutputFile.puts ""
+  pOutputFile.puts "</tbody>"
+  pOutputFile.puts "</table>"
 end
 
-createMappingTable( "2003 Revision of the U.S. Standard Certificate of Live Birth", vOutputFile, vSpreadsheet)
-createMappingTable( "2016 US Standard Attachment to the Facility Worksheet for the Live Birth Certificate for Multiple Births", vOutputFile, vSpreadsheet)
-createMappingTable( "2016 US Standard Facility Worksheet for the Live Birth Certificate", vOutputFile, vSpreadsheet)
-createMappingTable( "2016 US Standard Mothers Worksheet for Child’s Birth Certificate", vOutputFile, vSpreadsheet)
+createMappingTable( "U.S. Standard Certificate of Death", "https://www.cdc.gov/nchs/data/dvs/DEATH11-03final-ACC.pdf", vOutputFile, vSpreadsheet)
 
-createMappingTableQaire( "2016 US Standard Mothers Worksheet for Child’s Birth Certificate", vOutputFile, vSpreadsheet)
-
-createMappingTable( "2003 Revision of the U.S. Standard Report of Fetal Death", vOutputFile, vSpreadsheet)
-createMappingTable( "2019 US Standard Facility Worksheet for the Report of Fetal Death", vOutputFile, vSpreadsheet)
-createMappingTable( "2019 US Standard Patient’s Worksheet for the Report of Fetal Death", vOutputFile, vSpreadsheet)
-
-createMappingTableQaire( "2019 US Standard Patient’s Worksheet for the Report of Fetal Death", vOutputFile, vSpreadsheet)
